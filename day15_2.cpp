437e02092783079687fefc840cbb081bb7cfe318
Ahmet Ala <44473668+alaahmet@users.noreply.github.com>
2021-12-15 18:07:42 +0100

    day15

---
+++ day15_2.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+const long long mx=1e6+6;
+const int n=100;
+int ax[]={-1,1,0,0};
+int ay[]={0,0,1,-1};
+int32_t main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);	
+	freopen("input.txt","r",stdin);
+	
+	//INPUT
+	long long dp[5*n+1][5*n+1];
+	for(int i=0;i<5*n+1;++i)for(int j=0;j<5*n+1;j++)dp[i][j]=1e15;
+	long long a[5*n+1][5*n+1];
+	for(int i=0;i<n;++i){
+		string s;cin >> s;
+		for(int j=0;j<s.length();++j){
+			a[i][j]=s[j]-'0';
+		}
+	}
+	//INPUT
+
+	// SCALE THE GRID *5
+	for(int i=0;i<5*n;++i){
+		for(int j=0;j<5*n;++j){
+			if(i<n and j<n)a[i][j]=a[i][j];
+			else if(i<n and j>=n)a[i][j]=a[i][j-n]+1;
+			else a[i][j]=a[i-n][j]+1;
+			a[i][j]=a[i][j]%10;
+			if(a[i][j]==0)a[i][j]++;
+		}
+	}
+	// SCALE THE GRID *5
+
+
+	//DIJKSTRA
+	priority_queue < pair <int, pair<int,int>> > pq;
+	pq.push(make_pair(0,make_pair(0,0)));
+	dp[0][0]=0;
+	while(!pq.empty()){
+		int dis = -pq.top().first;
+		int x=pq.top().second.first;
+		int y=pq.top().second.second; 
+		pq.pop();
+		if(dis!=dp[x][y])continue;
+		for(int i=0;i<4;++i){
+			if(x+ax[i]>=5*n or y+ay[i]>=5*n or x+ax[i]<0 or y+ay[i]<0)continue;
+			int ndis=dis+a[x+ax[i]][y+ay[i]];
+			if(ndis>=dp[x+ax[i]][y+ay[i]])continue;
+			dp[x+ax[i]][y+ay[i]]=ndis;
+			pq.push(make_pair(-ndis,make_pair(x+ax[i],y+ay[i])));
+		}
+	}
+	//DIJKSTRA
+	
+	cout << dp[5*n-1][5*n-1] << endl;
+}
+
+
